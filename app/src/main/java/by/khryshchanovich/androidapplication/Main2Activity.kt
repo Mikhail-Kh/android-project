dc18b1b13fdab11ff55ebde877eceda96104bcb1
Mikhail-Kh <mig86h@tut.by>
2020-09-21 23:52:28 +0300

    Add image upload

--- app/src/main/java/by/khryshchanovich/androidapplication/Main2Activity.kt
+++ app/src/main/java/by/khryshchanovich/androidapplication/Main2Activity.kt
@@ -2,10 +2,18 @@ package by.khryshchanovich.androidapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        buttonLoad.setOnClickListener {
+            val url:String = editText.text.toString()
+            Picasso.get().load(url).into(imageView)
+        }
     }
 }
