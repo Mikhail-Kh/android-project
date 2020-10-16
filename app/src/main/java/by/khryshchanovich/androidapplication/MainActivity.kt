e56b30b5da564db95bd66b46b806130c21d72001
Mikhail-Kh <mig86h@tut.by>
2020-10-17 00:09:35 +0300

    Create the application for Mother's Day

--- app/src/main/java/by/khryshchanovich/androidapplication/MainActivity.kt
+++ app/src/main/java/by/khryshchanovich/androidapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package by.khryshchanovich.androidapplication
 import android.content.Intent  // подключаем класс Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.khryshchanovich.androidapplication.mothers_day.MainStartActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +29,10 @@ class MainActivity : AppCompatActivity() {
         button3.setOnClickListener {
             val myIntent = Intent(this, Main6Activity::class.java)
             startActivity(myIntent)
+        }
+
+        button_mothers_day.setOnClickListener {
+            startActivity(Intent(this, MainStartActivity::class.java))
         }
     }
 }
