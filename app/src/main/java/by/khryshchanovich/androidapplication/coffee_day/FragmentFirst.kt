00474f539932d91a4255f85720200423bbaa4224
Mikhail-Kh <mig86h@tut.by>
2020-10-20 15:17:15 +0300

    I removed the autogenerated code

--- app/src/main/java/by/khryshchanovich/androidapplication/coffee_day/FragmentFirst.kt
+++ app/src/main/java/by/khryshchanovich/androidapplication/coffee_day/FragmentFirst.kt
@@ -9,34 +9,11 @@ import android.widget.Toast
 import by.khryshchanovich.androidapplication.R
 import kotlinx.android.synthetic.main.fragment_first.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentFirst.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentFirst : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
@@ -64,25 +41,5 @@ class FragmentFirst : Fragment() {
             edit_price_of_coffee.text.clear()
             edit_url_of_coffee.text.clear()
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentFirst.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentFirst().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
