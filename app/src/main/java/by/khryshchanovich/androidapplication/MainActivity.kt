ba053b53b06816f134198874817e4f9e1cdcb38c
Mikhail Khryshchanovich <61590423+Mikhail-Kh@users.noreply.github.com>
2020-11-22 18:06:44 +0300

    Update MainActivity.kt
    Fix MainActivity

--- app/src/main/java/by/khryshchanovich/androidapplication/MainActivity.kt
+++ app/src/main/java/by/khryshchanovich/androidapplication/MainActivity.kt
@@ -1,6 +1,6 @@
 package by.khryshchanovich.androidapplication
 
-import android.content.Intent  // подключаем класс Intent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,13 +14,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-<<<<<<< HEAD
-    override fun onStart() {
-        super.onStart()
-    }
-
-=======
->>>>>>> feature_load_picture
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
